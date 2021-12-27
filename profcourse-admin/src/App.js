61502df28ae3fd91834be1bb4ca1e13bcc8b74ff
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2021-12-27 22:44:12 +0700

    sidebar

--- profcourse-admin/src/App.js
+++ profcourse-admin/src/App.js
@@ -3,7 +3,40 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Dalam proses Pengerjaan</h1>
+      <div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-secondary">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
+                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline">Menu</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0">
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline fw-bold">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Kursus</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle">
+                            <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Pengguna</span></a>
+                    </li>
+                    <li>
+                        <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Permintaan</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col py-3">
+            Content area...
+        </div>
+    </div>
+</div>
+
     </div>
   );
 }
