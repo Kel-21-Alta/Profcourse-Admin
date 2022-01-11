3f66ed167f6cc758c9c48f54bbfdef024313d9cf
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2022-01-11 13:26:39 +0700

    update buat pengguna

--- profcourse-admin/src/pages/buat-pengguna.jsx
+++ profcourse-admin/src/pages/buat-pengguna.jsx
@@ -2,6 +2,7 @@ import BuatPenggunaPart from "../Components/Parts/buatPenggunaPart";
 import AdminTab from "../Components/Usable/adminTab";
 import Sidebar from "../Components/Usable/navbar";
 
+
 export default function BuatPengguna(){
     return (
         <div class="container-fluid">
