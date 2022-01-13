ed9e23c75f53e98e1354d00e8595975a1c1e04a6
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2022-01-13 16:33:34 +0700

    fix tabs with useState

--- profcourse-admin/src/Components/Parts/kursusPart.jsx
+++ profcourse-admin/src/Components/Parts/kursusPart.jsx
@@ -1,6 +1,12 @@
-import User from "../../assets/carbon_user-avatar-filled.png"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function KursusPart(props){
+  const [tabs,setTabs] = useState(1)
+
+  const toggleTab = (index) => {
+    setTabs(index)
+    console.log(tabs)
+  }
     return (
         <div className="mx-5 my-3">
             <h2 className="fw-bold">Kursus</h2>
@@ -8,24 +14,25 @@ export default function KursusPart(props){
                 <button className="btn btn-thirtiery shadow">Buat Kursus</button>
                 <Link to="buat"><button className="btn btn-thirtiery shadow">Buat Spesialisasi</button></Link>
             </div>
-            <ul class="nav nav-tabs" id="myTab" role="tablist">
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Kursus</button>
+    <button onClick={()=>toggleTab(1)} class={tabs === 1 ? "nav-link active" : "nav-link"} id="kursus-tab" data-bs-toggle="tab" data-bs-target="#kursus" type="button" role="tab" aria-controls="kursus" aria-selected="true">Kursus</button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Spesialisasi</button>
+    <button onClick={()=>toggleTab(2)} class={tabs === 2 ? "nav-link active" : "nav-link"} id="spesialisasi-tab" data-bs-toggle="tab" data-bs-target="#spesialisasi" type="button" role="tab" aria-controls="spesialisasi" aria-selected="false">Spesialisasi</button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Publik</button>
+    <button onClick={()=>toggleTab(3)} class={tabs === 3 ? "nav-link active" : "nav-link"} id="publik-tab" data-bs-toggle="tab" data-bs-target="#publik" type="button" role="tab" aria-controls="publik" aria-selected="false">Publik</button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Draf</button>
+    <button onClick={()=>toggleTab(4)} class={tabs === 4 ? "nav-link active" : "nav-link"} id="draf-tab" data-bs-toggle="tab" data-bs-target="#draf" type="button" role="tab" aria-controls="draf" aria-selected="false">Draf</button>
   </li>
 </ul>
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"></div>
-  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"></div>
-  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab"></div>
+  <div class={tabs === 1 ? "tab-pane fade show active" : "tab-pane fade"} id="kursus" role="tabpanel" aria-labelledby="kursus-tab">kursus</div>
+  <div class={tabs === 2 ? "tab-pane fade show active" : "tab-pane fade"} id="spesialisasi" role="tabpanel" aria-labelledby="spesialisasi-tab">spesialisasi</div>
+  <div class={tabs === 3 ? "tab-pane fade show active" : "tab-pane fade"} id="publik" role="tabpanel" aria-labelledby="publik-tab">publik</div>
+  <div class={tabs === 4 ? "tab-pane fade show active" : "tab-pane fade"} id="draf" role="tabpanel" aria-labelledby="draf-tab">draf</div>
 </div>
 
             <div className="d-flex my-2">
@@ -65,6 +72,5 @@ export default function KursusPart(props){
 </div>
 </div>
         </div>
-   
     );
 }
