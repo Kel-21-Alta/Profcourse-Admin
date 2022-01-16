d656a2ca65c2061ead8f0cfcd0a1a076decb44c9
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2022-01-16 22:52:27 +0700

    boilerplate page detail spesialisasi

--- profcourse-admin/src/App.js
+++ profcourse-admin/src/App.js
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import BuatPengguna from "./pages/buat-pengguna";
-import CreateKursus from "./pages/CreateKursus";
+import DetailKursus from "./pages/CreateKursus";
 import Dashboard from "./pages/dashboard";
+import DetailSpesialisasi from "./pages/detailSpesialisasi";
 import Kursus from "./pages/kursus";
 import Login from "./pages/login";
 import LupaPassword from "./pages/lupa-password";
@@ -14,9 +15,10 @@ function App() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/pengguna" element={<Pengguna />} />
       <Route path="/kursus" element={<Kursus />} />
+      <Route path="/kursus/spesialisasi" element={<DetailSpesialisasi />} />
       <Route path="/pengguna/buat" element={<BuatPengguna />} />
       <Route path="/lupa-password" element={<LupaPassword />} />
-      <Route path="/buat-kursus" element={<CreateKursus />} />
+      <Route path="/buat-kursus" element={<DetailKursus />} />
     </Routes>
   );
 }
