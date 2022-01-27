ab66827943ac2b7051dbd3ba030d2696c25b36cc
adrianstanislaus <18118036@telecom.stei.itb.ac.id>
2022-01-27 08:41:16 +0700

    add specialization + limit

---
+++ profcourse-admin/src/Components/Usable/spesialisasiTab.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import SpesialisasiCard from "../cards/spesialisasiCard";
+
+export default function SpesialisasiTab(props) {
+  console.log(props?.data);
+  return (
+    <div>
+      <div className="container mx-3 p-0">
+        <div className="row row-cols-1 row-cols-md-3 gap-5 text-center">
+          {props?.data?.map((item) => (
+            <SpesialisasiCard key={item.id} data={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
